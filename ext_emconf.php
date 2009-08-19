deab3c1021999a9728a037e5a4c4d205679ad4e2
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-19 06:21:04 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkihateie/trunk@23471 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "pmkihateie"
+#
+# Auto generated 18-08-2009 20:37
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'PMK I hate IE',
+	'description' => 'Displays a fake IE "infobar" on Internet Explorer browsers (Similar to the annoying ActiveX infobar) with user defined text and link.
+Can be used to try persuading IE6 users to upgrade. Customizable "Hate level", so you can decide which IE versions should display the infobar. On non-IE browsers the script is not loaded.
+Written in Typoscript/Javascript. - No PHP. ;)',
+	'category' => 'fe',
+	'author' => 'Peter Klein',
+	'author_email' => 'pmk@io.dk',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"d0ad";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"29ea";s:19:"doc/wizard_form.dat";s:4:"692b";s:20:"doc/wizard_form.html";s:4:"8599";s:30:"static/i_hate_ie/constants.txt";s:4:"d41d";s:26:"static/i_hate_ie/setup.txt";s:4:"d41d";}',
+);
+
+?>
---
+++ res/ihateie.js
@@ -0,0 +1,121 @@
+/***************************************************************
+*
+*  PMK I hate IE
+*
+*  Copyright notice
+*
+*  (c) 2009 Peter Klein  (pmk@io.dk)
+*  All rights reserved
+*
+*  Released under the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+*  This copyright notice MUST APPEAR in all copies of this script
+*
+***************************************************************
+* Displays a fake IE "infobar" on Internet Explorer browsers
+* (Similar to the annoying ActiveX infobar) with user defined text and link.
+* Can be used to try persuading IE6 users to upgrade.
+* Customizable "Hate level", so you can decide which IE versions
+* should display the infobar.
+***************************************************************/
+
+function ihateie(options) {
+	// Versions of IE below or equal to this value, triggers the hatebar
+	var hatelevel = options.hatelevel || 6;
+//alert(options.hatelevel);
+
+	var resizehandler = function() {
+		var width = document.documentElement.clientWidth || document.body.clientWidth;
+		// This is a perefect example of why IE SUCKS!!
+		// The resize event is attached to the window object, so you would assume that the event fires
+		// when the browserwindow is resized. It does, but it also fires the event on ALL elements
+		// on the page, making it running an endless loop which crashes the browser.
+		// To fix that, you need to check if it's really the browserwindow width/height that have changed.
+		if(this.curwidth != width) {
+			document.getElementById("iehatebar").style.width = width;
+		}
+		this.curwidth = width;
+	}
+
+	var closebar = function() {
+		window.detachEvent('onresize',resizehandler);
+		window.detachEvent('onclick',closebar);
+		document.body.removeChild(document.getElementById("iehatebar"))
+	}
+		
+	// Find IE Browser version
+	var bv = (navigator.appVersion.indexOf("MSIE") != -1) ? parseFloat(navigator.appVersion.split("MSIE")[1]): 999;
+
+	if (bv<=hatelevel) {
+		if (bv<=6) {
+			// Really old crap
+			var msg = options.ie6text || "Your web browser is outdated! Click here for information on upgrading to a better browser."
+			var href = options.ie6link || "http://www.mozilla.com/firefox/";
+			var title = options.ie6title || "Your web browser is outdated!"
+			var image = options.ie6icon || "typo3conf/ext/pmkihateie/res/warning.gif";
+		};
+		else if (bv==7) {
+			// Old crap
+			var msg = options.ie7text || "You are using an inferior browser. You should consider changing to a better browser, such as: Firefox, Safari, Opera or Chrome."
+			var href = options.ie7link || "http://www.mozilla.com/firefox/";
+			var title = options.ie7title || "You are using an inferior browser."
+			var image = options.ie7icon || "typo3conf/ext/pmkihateie/res/warning.gif";
+		}
+		else if (bv>7) {
+			// New crap
+			var msg = options.ie8text || "You are using an inferior browser. You should consider changing to a better browser, such as: Firefox, Safari, Opera or Chrome."
+			var href = options.ie8link || "http://mashable.com/2009/07/16/ie6-must-die/";
+			var title = options.ie8title || "You are using an inferior browser."
+			var image = options.ie8icon || "typo3conf/ext/pmkihateie/res/warning.gif";
+		}
+
+		// Create DIV container for the bar
+    	var iehatebar = document.createElement("div");
+		iehatebar.id ="iehatebar";
+		iehatebar.style.width = (document.documentElement.clientWidth || document.body.clientWidth);
+		iehatebar.style.marginTop = -parseInt(document.body.currentStyle.marginTop);
+		iehatebar.style.marginRight = -parseInt(document.body.currentStyle.marginRight);
+		iehatebar.style.marginBottom = parseInt(document.body.currentStyle.marginBottom);
+		iehatebar.style.marginLeft = -parseInt(document.body.currentStyle.marginLeft);
+		document.body.insertBefore(iehatebar,document.body.firstChild);
+		
+		// Create A tag for the infotext in the bar
+		var iehatebartext = document.createElement("a");
+		iehatebartext.appendChild(document.createTextNode(msg));
+		iehatebartext.id ="iehatebartext";
+		iehatebartext.href = href;
+		iehatebartext.target = "_blank";
+		iehatebartext.title = title;
+		iehatebartext.style.backgroundImage = "url("+image+")";
+		iehatebar.appendChild(iehatebartext);
+		
+		// Create A tag for the closebutton in the bar
+		var iehatebarclose = document.createElement("a");
+		iehatebarclose.id ="iehatebarclose";
+		iehatebarclose.title = options.closetitle || "Click to close infobar";
+		iehatebarclose.attachEvent('onclick',closebar);
+		iehatebar.appendChild(iehatebarclose);
+		
+		// Attach Resizehandler
+		window.attachEvent('onresize',resizehandler);
+
+		// Cleanup to prevent memory leaks
+		iehatebarclose.attachEvent('onunload',closebar);
+
+		// Just a quickie animation using IE filter transition.
+		// Unfortunatly there's no filter for pushing down content.
+		iehatebar.filters.item(0).Apply();
+		iehatebar.style.display="block";
+		iehatebar.filters.item(0).Play();
+	}
+}
+
