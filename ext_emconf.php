24ad21257ac00c8b23d2951d8a44320340194ade
pmk65 <pmk65@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-08-21 07:58:05 +0000

    Now working on TYPO3 versions below 4.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pmkihateie/trunk@23541 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "pmkihateie"
 #
-# Auto generated 20-08-2009 22:04
+# Auto generated 21-08-2009 09:27
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -16,22 +16,24 @@ $EM_CONF[$_EXTKEY] = array(
 Could be used to try persuading IE6 users to upgrade. Customizable "Hate level", so you can decide which IE versions should display the infobar. On non-IE browsers the script is not loaded.
 Written in unobtrusive Javascript. - No PHP. ;)',
 	'category' => 'fe',
-	'author' => 'Peter Klein',
-	'author_email' => 'pmk@io.dk',
-	'shy' => '',
+	'shy' => 0,
+	'version' => '0.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'beta',
-	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
+	'author' => 'Peter Klein',
+	'author_email' => 'pmk@io.dk',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -41,8 +43,6 @@ Written in unobtrusive Javascript. - No PHP. ;)',
 		),
 	),
 	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"d0ad";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"0580";s:14:"ext_tables.php";s:4:"29ea";s:14:"doc/manual.sxw";s:4:"b3e6";s:12:"res/bomb.gif";s:4:"3f72";s:13:"res/close.gif";s:4:"93e2";s:15:"res/ihateie.css";s:4:"8d4d";s:14:"res/ihateie.js";s:4:"b1c0";s:12:"res/noie.gif";s:4:"dbde";s:15:"res/warning.gif";s:4:"6706";s:30:"static/i_hate_ie/constants.txt";s:4:"d3f7";s:26:"static/i_hate_ie/setup.txt";s:4:"072e";}',
-	'suggests' => array(
-	),
 );
 
 ?>
